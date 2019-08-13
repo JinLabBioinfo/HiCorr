00ecd410bd8f36f842b3bd187e09f1a6b84c6b7c
JinLab <53835021+JinLabBioinfo@users.noreply.github.com>
2019-08-13 10:42:00 -0700

    Update prep_ref.sh

--- ref/prep_ref.sh
+++ ref/prep_ref.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./list_frag_pairs.pl hg19.HindIII.frag.bed 2000000 | ./remove_outlier.py hg19.blacklist.frag >hg19.within_2Mb.frag_pair
-./count_trans_pairs_by_length.pl hg19.HindIII.frag.bed group.frag_length.range hg19.frag.length.map >trans.group.count.by.length
-./count_trans_pairs_by_length.pl hg19.HindIII.frag.bed group.frag_GC.range hg19.frag.gc.map >trans.group.count.by.GC
-./get_map_5kb_window.pl hg19.HindIII.frag.bed >frag.2.all.5kb.anchor
+genome=$1
+
+./list_frag_pairs.pl $genome.HindIII.frag.bed 2000000 | ./remove_outlier.py $genome.blacklist.frag >$genome.within_2Mb.frag_pair
+./count_trans_pairs_by_length.pl $genome.HindIII.frag.bed $genome.group.frag_length.range $genome.frag.length.map >trans.group.count.by.length
+./count_trans_pairs_by_length.pl $genome.HindIII.frag.bed $genome.group.frag_GC.range $genome.frag.gc.map >trans.group.count.by.GC
+./get_map_5kb_window.pl $genome.HindIII.frag.bed >frag.2.all.5kb.anchor

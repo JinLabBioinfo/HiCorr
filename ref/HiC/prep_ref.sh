918069de19314d55657236a7a7dacbc35be7c0e6
JinLab <53835021+JinLabBioinfo@users.noreply.github.com>
2020-08-06 17:40:05 -0700

    Update prep_ref.sh

--- ref/HiC/prep_ref.sh
+++ ref/HiC/prep_ref.sh
@@ -2,4 +2,4 @@
 
 genome=$1
 
-./list_frag_pairs.pl $genome.HindIII.frag.bed 2000000 | ./remove_outlier.py $genome.blacklist.frag >$genome.frag_pairs.within_2Mb.without.blacklist
+perl list_frag_pairs.pl $genome.HindIII.frag.bed 2000000 | python remove_outlier.py $genome.blacklist.frag >$genome.frag_pairs.within_2Mb.without.blacklist

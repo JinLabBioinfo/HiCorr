c9558a496690b3ea11e4c8f8b602af7760aa43da
JinLab <53835021+JinLabBioinfo@users.noreply.github.com>
2020-08-06 18:23:28 -0700

    Add files via upload

---
+++ bin/HindIII/get_anchor_pval.r
@@ -0,0 +1,12 @@
+# NEED TO UPDATE THIS SLOPE VALUE EVERY TIME
+slope <- 2
+
+data <- read.table("FILE", col.names=c("gid1", "gid2", "val", "rand"))
+attach(data)
+prob <- 1 - 1/slope
+r <- rand / (slope - 1)
+r[r==0] <- 1e5
+data$pval <- pnbinom(val - 1, size=r, mu=rand, lower.tail=FALSE)
+detach()
+write.table(data, file="FILE.p_val", row.names=FALSE, col.names=FALSE, sep="\t", quote=FALSE)
+

3660087d23484cf4496d93399b2bd14c09cadb57
Shanshan Zhang <39412563+shanshan950@users.noreply.github.com>
2022-10-17 22:40:19 -0400

    Update HiCorr_micro-C.sh

--- HiCorr_micro-C.sh
+++ HiCorr_micro-C.sh
@@ -9,13 +9,16 @@ genome=$6
 anchorbed=${ref}/$genome.5kb.bed
 blacklist=${ref}/$genome.5kb.bed.blacklist
 lib=$bin
-#Python 2.7.5
+#
+# fragment pairs to anchorpairs 
+$lib/fragdata_to_anchordata.pl $cis $ref/hg19.500bp_5kb | $lib/remove.blacklist.py $blacklist | $lib/get_dist.py $anchorbed > end_loop.$name.cis &
+$lib/fragdata_to_anchordata.pl $trans $ref/hg19.500bp_5kb | $lib/remove.blacklist.py $blacklist > end_loop.$name.trans &
+wait 
 
-cat $cis | $lib/remove.blacklist.py $blacklist |awk '{if($4<=2000000) print $1,$2,$3,$4}' OFS='\t' | $lib/split_chromo.py $anchorbed
-
-cat $cis  | $lib/remove.blacklist.py $blacklist | awk '{if($4<=2000000) print $1,$2,$3,$4}' OFS='\t' | $lib/get_group_statistics.pl - $ref/$genome.dist.5kb.group > dist.stat
-
-cat $cis  | $lib/remove.blacklist.py $blacklist | awk '{if($4>2000000) print $0}' | cat - $trans | cut -f1-3 | $lib/calculate_vis.py > anchor.vis.list 
+cat end_loop.$name.cis | awk '{if($4<=2000000) print $1,$2,$3,$4}' OFS='\t' | $lib/split_chromo.py $anchorbed & 
+cat end_loop.$name.cis | awk '{if($4<=2000000) print $1,$2,$3,$4}' OFS='\t' | $lib/get_group_statistics.pl - $ref/$genome.dist.5kb.group > dist.stat & 
+cat end_loop.$name.cis | awk '{if($4>2000000) print $0}' | cat - end_loop.$name.trans | cut -f1-3 | $lib/calculate_vis.py > anchor.vis.list &
+wait
 
 Rscript $lib/integrated.r $ref/$genome.full.dist.stat.5kb
 

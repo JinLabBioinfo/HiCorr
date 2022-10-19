fef5a8d7cd781221d2984de9f8a7ecc2a6deefc2
JinLab <53835021+JinLabBioinfo@users.noreply.github.com>
2022-10-19 15:01:21 -0400

    Update sites_to_frag.py

--- bin/generateReference_lib/sites_to_frag.py
+++ bin/generateReference_lib/sites_to_frag.py
@@ -16,19 +16,19 @@ with open(sys.argv[2],'r') as f:
 	for line in f:
 		chr,beg,end=line.rstrip().split('\t')
 		if prev=="":
-			print chr+'\t'+'1'+'\t'+str(int(beg)+1)+'\t'+'frag_'+str(i)
+			print(chr+'\t'+'1'+'\t'+str(int(beg)+1)+'\t'+'frag_'+str(i))
 			i+=1
 		if prev==chr:
-			print chr+'\t'+str(prev_end-1)+'\t'+str(int(beg)+1)+'\t'+'frag_'+str(i)			
+			print(chr+'\t'+str(prev_end-1)+'\t'+str(int(beg)+1)+'\t'+'frag_'+str(i))
 			i+=1
 		if prev!=chr and prev!="":
-			print prev+'\t'+str(prev_end-1)+'\t'+str(chromosize[prev])+'\t'+'frag_'+str(i)
+			print(prev+'\t'+str(prev_end-1)+'\t'+str(chromosize[prev])+'\t'+'frag_'+str(i))
 			i+=1
-			print chr+'\t'+'1'+'\t'+str(int(beg)+1)+'\t'+'frag_'+str(i)
+			print(chr+'\t'+'1'+'\t'+str(int(beg)+1)+'\t'+'frag_'+str(i))
 			i+=1
 		prev=chr
 		prev_end=int(end)
 		
-print prev+'\t'+str(prev_end-1)+'\t'+str(chromosize[prev])+'\t'+'frag_'+str(i)
+print(prev+'\t'+str(prev_end-1)+'\t'+str(chromosize[prev])+'\t'+'frag_'+str(i))
 
 f.close()

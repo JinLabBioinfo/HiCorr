90465cbdde5b96d77b96b978834b4f622e3318f7
JinLab <53835021+JinLabBioinfo@users.noreply.github.com>
2022-12-02 12:27:10 -0500

    fix find_RE.pl

--- bin/generateReference_lib/find_RE_sites.pl
+++ bin/generateReference_lib/find_RE_sites.pl
@@ -2,22 +2,22 @@
 
 use strict;
 
-my $usage = 	"Usage:./find_RE_sites.pl <genome> <sequence>\n".
+my $usage = 	"Usage:./find_RE_sites.pl <genome_fa> <genome_size> <sequence> <generateLibPath>\n".
 		"\tThis program find <sequence> sites in <genome>\n".
 		"\tOutput is in 1-system.\n";
 
-my ($genome, $seq) = @ARGV;
-if(not defined $seq){
-	die($usage);
-}
+my ($genome_fa_dir, $genome_chrom_size, $cutsite,$lib_path) = @ARGV;
+#if(not defined $seq){
+#	die($usage);
+#}
 
 #my $home = `echo \$HOME`;
 #chomp $home;
 #my $folder="/mnt/NFS/geneITS0/JinLab/fxj45/GenebankDB/$genome";
-if($genome=="hg19"){
-open(IN, "/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/hg19/Homo_sapiens/UCSC/hg19/Sequence/Chromosomes/chrom.sizes");
-}else{
-open(IN, "/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/mm10_bowtie_index/mm10.chrom.sizes");}
+#if($genome=="hg19"){
+open(IN, $genome_chrom_size);
+#}else{
+#open(IN, "/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/mm10_bowtie_index/mm10.chrom.sizes");}
 my $sizeref;
 while(my $line = <IN>){
 	my ($chr, $size) = split "\t", $line;
@@ -27,17 +27,17 @@ while(my $line = <IN>){
 	$sizeref->{$chr} = $size;
 }
 close(IN);
-my $fa_dir;
-if($genome=="mm10"){
-	$fa_dir = "/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/mm10_bowtie_index/";
-}else{
-	$fa_dir = "/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/hg19/Homo_sapiens/UCSC/hg19/Sequence/Chromosomes/";}
+#my $fa_dir;
+#if($genome=="mm10"){
+#	$fa_dir = "/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/mm10_bowtie_index/";
+#}else{
+#	$fa_dir = "/mnt/rstor/genetics/JinLab/xxl244/Reference_Indexes/hg19/Homo_sapiens/UCSC/hg19/Sequence/Chromosomes/";}
 
 foreach my $chr (sort keys %{$sizeref}){
 	my $size = $sizeref->{$chr};
-	my @pos = `/mnt/rds/genetics01/JinLab/xww/HiCorr/prepare_files.pip/src/sequence_match.pl -c $fa_dir/$chr.fa $seq`;
+	my @pos = `$lib_path/sequence_match.pl -c $genome_fa_dir/$chr.fa $cutsite`;
 	chomp @pos;
-	my $len = length($seq);
+	my $len = length($cutsite);
 	while(my $loc = shift @pos){
 		print join("\t", $chr, $loc, $loc + $len - 1)."\n";
 	}

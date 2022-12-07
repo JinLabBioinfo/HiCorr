fb12a17b3756bb5b1684e6168b4c97dfa3955ef0
JinLab <53835021+JinLabBioinfo@users.noreply.github.com>
2022-12-06 19:39:28 -0500

    Update get_group_statistics.pl

--- bin/generateReference_lib/get_group_statistics.pl
+++ bin/generateReference_lib/get_group_statistics.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 
 use strict;
-my ($regress, $frag_bed, $len_group_file, $dist_group_file, $frag_stat_file) = @ARGV;
+my ($regress, $anchor_bed, $len_group_file, $dist_group_file) = @ARGV;
 if(not defined $dist_group_file){
-	die("Usage:./get_group_statistics.pl <frag_loop_data> <frag bed> <frag_length_group> <frag_dist_group> <frag_stat>\n");
+	die("Usage:./get_group_statistics.pl <anchor_loop_data> <anchor bed> <anchor_length_group> <anchor_dist_group>\n");
 }
 
 ##################### determine the length group for fragments #################
@@ -17,23 +17,11 @@ while(my $line = <IN>){
 close(IN);
 
 my $frag_len_group;
-open(IN, $frag_bed);
+open(IN, $anchor_bed);
 while(my $line = <IN>){
 	chomp $line;
-	my ($chr, $beg, $end, $id, $len) = split "\t", $line;
+	my ($chr, $beg, $end, $id,$dist ,$len) = split "\t", $line;
 	$frag_len_group->{$id} = get_id($len, $len_range);
-}
-close(IN);
-
-##################### get the fragment stat information ###########################
-my $frag_gc;
-my $frag_map;
-open(IN, $frag_stat_file);
-while(my $line=<IN>){
-	chomp $line;
-	my ($id, $gc, $map) = split "\t", $line;
-	$frag_gc->{$id} = $gc;
-	$frag_map->{$id} = $map;
 }
 close(IN);
 
@@ -56,9 +44,9 @@ my $g_count;
 open(IN, $regress);
 while(my $line = <IN>){
 	chomp $line;
-	my ($frag1, $frag2, $count, $dist) = split "\t", $line;
-	my $g1 = $frag_len_group->{$frag1};
-	my $g2 = $frag_len_group->{$frag2};
+	my ($anchor1, $anchor2, $count, $dist) = split "\t", $line;
+	my $g1 = $frag_len_group->{$anchor1};
+	my $g2 = $frag_len_group->{$anchor2};
 	my $g3 = $dist_group[$dist];
 	if(not defined $g_sum->{$g1}->{$g2}->{$g3}){
 		$g_sum->{$g1}->{$g2}->{$g3} = 0;
@@ -67,12 +55,11 @@ while(my $line = <IN>){
                 $g_count->{$g1}->{$g2}->{$g3} = 0;
         }
 
-	my $map1 = $frag_map->{$frag1};
-	my $map2 = $frag_map->{$frag2};
-	if($map1 > 0.2 && $map2 > 0.2){
-		$g_count->{$g1}->{$g2}->{$g3} ++;
-		$g_sum->{$g1}->{$g2}->{$g3} += $count / $map1 / $map2;
-	}
+
+	
+	$g_count->{$g1}->{$g2}->{$g3} ++;
+	$g_sum->{$g1}->{$g2}->{$g3} += $count
+	
 
 }
 close(IN);
@@ -82,11 +69,20 @@ foreach my $g1 (sort {$a<=>$b} keys %{$g_sum}){
 		foreach my $g3 (sort {$a<=>$b} keys %{$g_sum->{$g1}->{$g2}}){
 			my $count = $g_count->{$g1}->{$g2}->{$g3};
 			my $sum = $g_sum->{$g1}->{$g2}->{$g3};
-			my $avg = $sum / $count;
-			print join("\t", $g1, $g2, $g3, $count, $avg)."\n";
+			if($count != 0){
+				my $avg = $sum / $count;
+				print join("\t", $g1, $g2, $g3, $count, $avg)."\n";
+			}else{
+				print join("\t", $g1, $g2, $g3, $count, "NA")."\n";
+			}
 		}
 	}
 }
+
+exit;
+
+
+##############################################################
 sub get_id{
         my ($val, $range) = @_;
         foreach my $id (keys %{$range}){
@@ -97,6 +93,3 @@ sub get_id{
         }
         die("Error: did not find a group\n");
 }
-
-exit;
-
